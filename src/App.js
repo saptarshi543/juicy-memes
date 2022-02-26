b063d28faed6067bfa0830c21ba2100e408bcdc4
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-02-26 18:18:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Show from './Show.js';
 function App() {
   return (
     <div className="App">
-      <h2>Some memes to help you go through your day</h2>
+      <h2>Some memes to help you ease your anxiety </h2>
       <br></br>
       <Show />
       <br></br>
