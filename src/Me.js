198dc13c5c686f1375641df906995271a0aef978
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-12-22 20:02:30 +0530

    modified about me

--- src/Me.js
+++ src/Me.js
@@ -12,7 +12,7 @@ const text={
             <h4>my E-mail: saptarshichatterjee543@gmail.com</h4>
         <a style={text} href='https://github.com/saptarshi543'>my github account</a>
         <br></br>
-        <p1>Wonder why this site does not have a nice name?
+        <p1>Wonder why this site does not have a nice domain name?
             That's because i am too poor to afford a site name.
             So <a style={text} href="https://www.buymeacoffee.com/sap543">pleasse support me here.</a>
         </p1>
